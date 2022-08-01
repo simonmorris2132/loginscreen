edac64d43b743d558d8dfe3676b5bd2c909cf37c
Simon Morris <simonjmorris213@gmail.com>
2022-07-31 22:11:02 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 import { useState } from 'react';
+import LoginForm from './components/LoginForm';
 
 function App() {
   
@@ -28,7 +28,7 @@ function App() {
           <button>Logout</button>
         </div>
       ) : (
-        
+        <LoginForm/>
       )}
     </div>
   );
